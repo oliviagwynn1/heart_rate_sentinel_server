6b60a519e46fab4dba9a669f3c6a964334512e41
Olivia Gwynn <oliviagwynn@duke.edu>
2018-11-17 15:17:47 -0500

    Updated server to include modified tachycardic app route and new interval_avg app route.

--- server.py
+++ server.py
@@ -69,6 +69,7 @@ def is_tachycardic(user_id):
     from tachy_conditions import tachycardic_conditions
     user_id = int(user_id)
     connect("mongodb://oliviagwynn1:GODUKE10@ds157503.mlab.com:57503/bme590")
+
     user = Patient.objects.raw({"_id": user_id}).first()
     age = user.user_age
     last_heart_rate = user.heart_rate_data[-1]["hr"]
