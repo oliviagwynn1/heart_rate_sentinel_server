331e91385f8ca6b94547860400c223c2efb18015
Olivia Gwynn <oliviagwynn@duke.edu>
2018-11-17 15:14:59 -0500

    Unit testing for tachycardic conditions working.

--- test_tachy_conditions.py
+++ test_tachy_conditions.py
@@ -2,8 +2,8 @@
 def test_tachycardic_conditions():
     from tachy_conditions import tachycardic_conditions
 
-    response = tachycardic_conditions(user_id=2, age=3, last_heart_rate=[145])
-    assert response == "Patient 2 is Tachycardic."
+    response = tachycardic_conditions(user_id=int(2), age=int(3), last_heart_rate=int(145))
+    assert response == "Patient {} is Tachycardic.".format(2)
 
 
 
