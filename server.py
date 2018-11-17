f5c25d8ab9d2d4263db655c47562750f6c198f27
Olivia Gwynn <oliviagwynn@duke.edu>
2018-11-17 13:37:23 -0500

    Merged tachycardic app route into main server.

--- server.py
+++ server.py
@@ -64,6 +64,18 @@ def list_heart_rates(user_id):
     return jsonify(user.heart_rate_data)
 
 
+@app.route("/api/status/<user_id>", methods=["GET"])
+def is_tachycardic(user_id):
+    from tachy_conditions import tachycardic_conditions
+    user_id = int(user_id)
+    connect("mongodb://oliviagwynn1:GODUKE10@ds157503.mlab.com:57503/bme590")
+    user = Patient.objects.raw({"_id": user_id}).first()
+    age = user.user_age
+    last_heart_rate = user.heart_rate_data[-1]["hr"]
+
+    return tachycardic_conditions(user_id, age, last_heart_rate)
+
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5001)
 
